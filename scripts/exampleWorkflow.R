06610dc0637090dd2aabbda54d806bfd89cff95a
rpique <rpique@gmail.com>
2014-07-28 18:25:10 -0400

    roxygenation yeag

--- scripts/exampleWorkflow.R
+++ scripts/exampleWorkflow.R
@@ -1,12 +1,20 @@
 ##################################################################
 ## QuASAR Example Workflow
 ##################################################################
-folderName="~/QuASAR/sampleinput/"
-fileNames=paste0(folderName,dir(folderName,"Et.*gz"))
 
 ##################################################################    
-## 1.) Prepare RNA-Seq samples
+## 1.) Download example pre-processed data
 ##################################################################
+
+
+urlData="http://genome.grid.wayne.edu/quasar/sampleinput/"
+fileNames <- paste0("EtOH",c(2,4,6,12,18,24),"hr_Huvec_Rep1.quasar.in.gz")
+sapply(fileNames,function (ii) download.file(paste0(urlData,ii),ii))
+
+##################################################################    
+## 2.) Load data into an R object
+##################################################################
+
 ase.dat <- UnionExtractFields(fileNames, combine=TRUE)
 ase.dat.gt <- PrepForGenotyping(ase.dat, min.coverage=5)
 str(ase.dat.gt)
@@ -16,8 +24,16 @@ sample.names <- colnames(ase.dat.gt$ref)
 ## 2.) QuASAR Model Fitting
 ## ase.joint ~ object for joint genotyoping across all samples
 ##################################################################
+
 ase.joint <- fitAseNullMulti(ase.dat.gt$ref, ase.dat.gt$alt, log.gmat=log(ase.dat.gt$gmat))
 str(ase.joint)
+
+## The ase.joint$gt object contains the posterior probability for each genotype: g0) is homozygote reference, g1) heterozygote, g2) homozygote alternate.
+head(ase.joint$gt)
+
+## The base-calling error parameters are stored in ase.joint$eps
+
+
 ## Saving the output genotype probabilities
 out_dat <- data.frame(ase.dat.gt$annotations[, -5], map=ase.joint$gt)
 write.table(out_dat, file='genotypes.txt', row.names=FALSE, col.names=FALSE, quote=FALSE,sep="\t")
