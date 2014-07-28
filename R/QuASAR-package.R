53958c30659342cc7c3154b01a68f9ce2f6fe772
ex7689 <ex7689@wayne.edu>
2014-07-28 14:00:20 -0400

    Added package script

---
+++ R/QuASAR-package.R
@@ -0,0 +1,21 @@
+#' Quantitative allele-specific analysis of reads
+#' 
+#' \tabular{ll}{
+#' Package: \tab QuASAR\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0\cr
+#' Date: \tab 28-07-2014\cr
+#' License: \tab GNU v3.0\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#' 
+#' Joint genotyping, error estimation, and Allele Specific Expression
+#' inference for multiple RNA-Seq samples.
+#' 
+#' @name QuASAR-package
+#' @aliases QuASAR
+#' @docType package
+#' @title Quantitative allele-specific analysis of reads
+#' @author Roger Pique-Regi \email{rpique@wayne.edu}, Chris Harvey \email{fl9788@wayne.edu}, Gregory Moyerbrailean \email{ex7689@wayne.edu}
+#' 
+roxygen()
