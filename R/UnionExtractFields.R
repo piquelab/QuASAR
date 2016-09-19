4cf04e2191f999fa4663d3100f1906001bf8ca1e
rpique <rpique@gmail.com>
2016-09-19 04:30:38 -0400

    some commits

--- R/UnionExtractFields.R
+++ R/UnionExtractFields.R
@@ -13,18 +13,18 @@
 UnionExtractFields <- function(fileList, combine=FALSE){			
 	#browser()
 	tmpFile <- scan(pipe("mktemp -t"),character(0))
-	system(paste("less ", paste(fileList, collapse=" "), " | grep -v -w '^chr\\|^NA' | cut -f 1-3,6-7 | sortBed -i stdin | uniq | gzip > ", tmpFile))
+	system(paste("less ", paste(fileList, collapse=" "), " | grep -v -w '^chr\\|^NA' | cut -f 1-7 | sortBed -i stdin | uniq | gzip > ", tmpFile))
 	anno <- read.table(gzfile(tmpFile),sep="\t",as.is=T)	
 	aux <- sapply(fileList,function(fn){			
 				#fn <- fileList[1]
 				cat("Processing:",fn,"\n")
-				command=paste("intersectBed -a ",tmpFile," -b ",fn," -wao | cut -f 1-3,13-15 ",sep="")
+				command=paste("intersectBed -a ",tmpFile," -b ",fn," -wao | cut -f 1-3,15-17 ",sep="")
 				aa <- read.table(pipe(command),sep="\t",as.is=T,na.strings=".")
 				aa[is.na(aa)] <- 0
 				stopifnot(identical(aa[,1:3],anno[,1:3]))
 				aa[,-(1:3)]
 			})
-	colnames(anno) = c("chr","pos0","pos", "rsID", "af")
+	colnames(anno) = c("chr","pos0","pos","ref","alt","rsID", "af")
 	Ref <- as.matrix(do.call(cbind,aux[1,]))
 	Alt <- as.matrix(do.call(cbind,aux[2,]))
 	Err <- as.matrix(do.call(cbind,aux[3,]))
