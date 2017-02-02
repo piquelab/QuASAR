8d933a55f61e9b7a3f6deb560acfe775366d3fee
rpique <rpique@gmail.com>
2017-02-02 17:35:17 -0500

    changes to mpra

--- mpra/mpra.R
+++ mpra/mpra.R
@@ -13,222 +13,80 @@
 ##It should be in an RData object, /wsu/home/fh/fh85/fh8591/piquelab/cindy/bitStarr/Tewhey/RNA/hepg2/summed.RNA.RData, LCL ind 1: ~/piquelab/cindy/bitStarr/Tewhey/summed.RNA.base.RData , and LCL ind 2: ~/piquelab/cindy/bitStarr/Tewhey/summed.RNA.base.LCL2.RData
 
 ##Loading LCL.1
-load("~/piquelab/cindy/bitStarr/Tewhey/summed.RNA.base.RData")
+##load("~/piquelab/cindy/bitStarr/Tewhey/summed.RNA.base.RData")
 
 
 library(QuASAR)
-#individual 1
+library(qqman)
+
+
+fitQuasarMpra <- function(ref,alt,prop=0.5,eps=0.001,nbreaks=10){
+  tot <- ref + alt;
+  cov_breaks <- unique(c(0,quantile(tot,(1:nbreaks)/nbreaks)))
+  bin <- cut(tot,cov_breaks)
+  M <- exp((0:500)/50)
+  ## aux ~ loglikelihood of the beta bionmial model across D values
+  ##               with null \rho value
+  ## Mmax ~ disperison which maximizes the llk
+  Mvec <- sapply(levels(bin),function(mybin){
+    aux <- sapply(M,function(M){
+      sum(logLikBetaBinomialRhoEps(prop[bin==mybin],eps,M,ref[bin==mybin ],alt[bin==mybin]))
+    })
+    Mmax <- M[which.max(aux)]
+    Mmax
+  })
+  #Mvec
+  #cat("#Disp: ", round(Mvec, 2), "\n")
+  aux2 <- t(sapply(1:length(ref),function(ii){
+    auxLogis <- optim(0,
+                      fn=logLikBetaBinomial2,
+                      gr=gLogLikBetaBinomial,
+                      D=Mvec[bin[ii]],
+                      R=ref[ii],
+                      A=alt[ii],
+                      method="L-BFGS-B",
+                      hessian=TRUE,
+                      lower=-5,
+                      upper=5)
+    c(auxLogis$par,1/(auxLogis$hessian)^.5)
+  }))
+  ## eps ~ jointly inferred error rate for this sample
+  rho3 <- plogis(aux2[,1])
+  betas.beta.binom <- aux2[,1]
+  lrt3 <-
+    logLikBetaBinomialRhoEps(rho3,eps,Mvec[bin],ref,alt) -
+    logLikBetaBinomialRhoEps(prop,eps,Mvec[bin],ref,alt)
+  pval3 <- (1-pchisq(2*lrt3,df=1))
+  ##
+  ##betas.se <- abs(betas.beta.binom/qnorm(pval3/2))
+  betas_se <- aux2[, 2]
+##  betas_w <- 1/betas_se^2
+  betas_z <- (betas.beta.binom - qlogis(prop))/betas_se
+  ##
+  padj_quasar <- p.adjust(pval3,method="BH")
+  data.frame(bin,betas.beta.binom,betas_se,betas_z,pval3,padj_quasar)
+}
+
+#individual 1 forward
 dd <- unique(mpra_Filt[,-c(3:12)])
-ref <- dd$R 
-alt <- dd$A
-tot <- ref + alt;
-prop <- dd$DNA_prop;
-eps <- 0.001
-cov_breaks <- unique(c(0,quantile(tot,(1:10)/10)))
-#cov_breaks
-bin <- cut(tot,cov_breaks)
-#table(bin)
-#tapply(tot,bin,mean)
-## M ~ a grid of possible dispersion values for the Beta-binomial model
-M <- exp((0:500)/50)
-## aux ~ loglikelihood of the beta bionmial model across D values
-##               with null \rho value
-## Mmax ~ disperison which maximizes the llk
-Mvec <- sapply(levels(bin),function(mybin){
-  aux <- sapply(M,function(M){
-    sum(logLikBetaBinomialRhoEps(prop[bin==mybin],eps,M,ref[bin==mybin ],alt[bin==mybin]))
-  })
-  Mmax <- M[which.max(aux)]
-  Mmax
-})
-#Mvec
-#cat("#Disp: ", round(Mvec, 2), "\n")
+aux <- fitQuasarMpra(dd$R,dd$A,dd$DNA_prop)
+zzz <- cbind(dd,aux)
 
-aux2 <- t(sapply(1:length(ref),function(ii){
-  auxLogis <- optim(0,
-                    fn=logLikBetaBinomial2,
-                    gr=gLogLikBetaBinomial,
-                    D=Mvec[bin[ii]],
-                    R=ref[ii],
-                    A=alt[ii],
-                    method="L-BFGS-B",
-                    hessian=TRUE,
-                    lower=-5,
-                    upper=5)
-  c(auxLogis$par,1/(auxLogis$hessian)^.5)
-}))
-## eps ~ jointly inferred error rate for this sample
-rho3 <- plogis(aux2[,1])
-betas.beta.binom <- aux2[,1]
-lrt3 <-
-  logLikBetaBinomialRhoEps(rho3,eps,Mvec[bin],ref,alt) -
-  logLikBetaBinomialRhoEps(prop,eps,Mvec[bin],ref,alt)
-pval3 <- (1-pchisq(2*lrt3,df=1))
-
-##betas.se <- abs(betas.beta.binom/qnorm(pval3/2))
-betas_se <- aux2[, 2]
-betas_w <- 1/betas_se^2
-betas_z <- (betas.beta.binom - qlogis(prop))/betas_se
-
-dd$bin <- cut(tot,cov_breaks)
-zzz <- cbind(dd,pval3,betas.beta.binom,betas_se,betas_w,betas_z)
-
-#reverse
+#individual 1 reverse
 dd <- unique(RC_Filt[,-c(2:11)])
-ref <- dd$R 
-alt <- dd$A
-tot <- ref + alt;
-prop <- dd$DNA_prop;
-eps <- 0.001
-cov_breaks <- unique(c(0,quantile(tot,(1:10)/10)))
-#cov_breaks
-bin <- cut(tot,cov_breaks)
-#table(bin)
-#tapply(tot,bin,mean)
+aux <- fitQuasarMpra(dd$R,dd$A,dd$DNA_prop)
+zzz_RC <- cbind(dd,aux)
 
-M <- exp((0:500)/50)
-Mvec <- sapply(levels(bin),function(mybin){
-  aux <- sapply(M,function(M){
-    sum(logLikBetaBinomialRhoEps(prop[bin==mybin],eps,M,ref[bin==mybin ],alt[bin==mybin]))
-  })
-  Mmax <- M[which.max(aux)]
-  Mmax
-})
-#Mvec
-#cat("#Disp: ", round(Mvec, 2), "\n")
-
-aux2 <- t(sapply(1:length(ref),function(ii){
-  auxLogis <- optim(0,
-                    fn=logLikBetaBinomial2,
-                    gr=gLogLikBetaBinomial,
-                    D=Mvec[bin[ii]],
-                    R=ref[ii],
-                    A=alt[ii],
-                    method="L-BFGS-B",
-                    hessian=TRUE,
-                    lower=-5,
-                    upper=5)
-  c(auxLogis$par,1/(auxLogis$hessian)^.5)
-}))
-
-rho3 <- plogis(aux2[,1])
-betas.beta.binom <- aux2[,1]
-lrt3 <-
-  logLikBetaBinomialRhoEps(rho3,eps,Mvec[bin],ref,alt) -
-  logLikBetaBinomialRhoEps(prop,eps,Mvec[bin],ref,alt)
-pval3 <- (1-pchisq(2*lrt3,df=1))
-betas_se <- aux2[, 2] #1/(auxLogis$hessian)^.5
-betas_w <- 1/betas_se^2
-betas_z <- (betas.beta.binom - qlogis(prop))/betas_se
-dd$bin <- cut(tot,cov_breaks)
-zzz_RC <- cbind(dd,pval3,betas.beta.binom,betas_se,betas_w,betas_z)
-
-zzz$padj_quasar <- p.adjust(zzz$pval3,method="BH")
-zzz_RC$padj_quasar <- p.adjust(zzz_RC$pval3,method="BH")
-
-#individual 2
+#individual 2 forward
 dd <- unique(mpra_2_Filt[,-c(2:7,10:11)])
-ref <- dd$R 
-alt <- dd$A
-tot <- ref + alt;
-prop <- dd$DNA_prop;
-eps <- 0.001
-cov_breaks <- unique(c(0,quantile(tot,(1:10)/10)))
-#cov_breaks
-bin <- cut(tot,cov_breaks)
-#table(bin)
-#tapply(tot,bin,mean)
+aux <- fitQuasarMpra(dd$R,dd$A,dd$DNA_prop)
+zzz_2 <- cbind(dd,aux)
 
-M <- exp((0:500)/50)
-Mvec <- sapply(levels(bin),function(mybin){
-  aux <- sapply(M,function(M){
-    sum(logLikBetaBinomialRhoEps(prop[bin==mybin],eps,M,ref[bin==mybin ],alt[bin==mybin]))
-  })
-  Mmax <- M[which.max(aux)]
-  Mmax
-})
-#Mvec
-cat("#Disp: ", round(Mvec, 2), "\n")
-
-aux2 <- t(sapply(1:length(ref),function(ii){
-  auxLogis <- optim(0,
-                    fn=logLikBetaBinomial2,
-                    gr=gLogLikBetaBinomial,
-                    D=Mvec[bin[ii]],
-                    R=ref[ii],
-                    A=alt[ii],
-                    method="L-BFGS-B",
-                    hessian=TRUE,
-                    lower=-5,
-                    upper=5)
-  c(auxLogis$par,1/(auxLogis$hessian)^.5)
-}))
-
-rho3 <- plogis(aux2[,1])
-betas.beta.binom <- aux2[,1]
-lrt3 <-
-  logLikBetaBinomialRhoEps(rho3,eps,Mvec[bin],ref,alt) -
-  logLikBetaBinomialRhoEps(prop,eps,Mvec[bin],ref,alt)
-pval3 <- (1-pchisq(2*lrt3,df=1))
-betas_se <- aux2[, 2]
-betas_w <- 1/betas_se^2
-betas_z <- (betas.beta.binom - qlogis(prop))/betas_se
-
-dd$bin <- cut(tot,cov_breaks)
-zzz_2 <- cbind(dd,pval3,betas.beta.binom,betas_se,betas_w,betas_z)
-
-#reverse
+# individual 2 reverse
 dd <- unique(RC_2_Filt[,-c(2:7,10:11)])
-ref <- dd$R 
-alt <- dd$A
-tot <- ref + alt;
-prop <- dd$DNA_prop;
-eps <- 0.001
-cov_breaks <- unique(c(0,quantile(tot,(1:10)/10)))
-#cov_breaks
-bin <- cut(tot,cov_breaks)
-#table(bin)
-#tapply(tot,bin,mean)
+aux <- fitQuasarMpra(dd$R,dd$A,dd$DNA_prop)
+zzz_RC_2 <- cbind(dd,aux)
 
-M <- exp((0:500)/50)
-Mvec <- sapply(levels(bin),function(mybin){
-  aux <- sapply(M,function(M){
-    sum(logLikBetaBinomialRhoEps(prop[bin==mybin],eps,M,ref[bin==mybin ],alt[bin==mybin]))
-  })
-  Mmax <- M[which.max(aux)]
-  Mmax
-})
-#Mvec
-cat("#Disp: ", round(Mvec, 2), "\n")
-
-aux2 <- t(sapply(1:length(ref),function(ii){
-  auxLogis <- optim(0,
-                    fn=logLikBetaBinomial2,
-                    gr=gLogLikBetaBinomial,
-                    D=Mvec[bin[ii]],
-                    R=ref[ii],
-                    A=alt[ii],
-                    method="L-BFGS-B",
-                    hessian=TRUE,
-                    lower=-5,
-                    upper=5)
-  c(auxLogis$par,1/(auxLogis$hessian)^.5)
-}))
-
-rho3 <- plogis(aux2[,1])
-betas.beta.binom <- aux2[,1]
-lrt3 <-
-  logLikBetaBinomialRhoEps(rho3,eps,Mvec[bin],ref,alt) -
-  logLikBetaBinomialRhoEps(prop,eps,Mvec[bin],ref,alt)
-pval3 <- (1-pchisq(2*lrt3,df=1))
-betas_se <- aux2[, 2]
-betas_w <- 1/betas_se^2
-betas_z <- (betas.beta.binom - qlogis(prop))/betas_se
-dd$bin <- cut(tot,cov_breaks)
-zzz_RC_2 <- cbind(dd,pval3,betas.beta.binom,betas_se,betas_w,betas_z)
-
-zzz_2$padj_quasar <- p.adjust(zzz_2$pval3,method="BH")
-zzz_RC_2$padj_quasar <- p.adjust(zzz_RC_2$pval3,method="BH")
 
 #combining individuals using fixed effects method
 LCL <- merge(zzz[,-c(6:8,12)], zzz_2[,-c(4:6,10)], by=c("rsID","alt_hap"), all=T)
